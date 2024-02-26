bf3049ff6d83345527676b51e326420110b76f03
Jack Seigerman <133878087+jackSeigerman@users.noreply.github.com>
2024-02-26 12:19:31 -0600

    Graphics Library

---
+++ graphicsLib/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cs1-graphics',
+    version='1.0',
+    packages=find_packages(),
+    description='A graphics library for CS1 courses.',
+    author='Jack Seigerman',
+    author_email='jacks3ds@gmail.com',
+    url='https://github.com/jackSeigerman/cs1-graphics',
+    install_requires=[
+        # List any dependencies required by your library
+
+    ],
+)
