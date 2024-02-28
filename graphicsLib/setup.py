3c7cb84baf1fd34ba232ed73167a798d0be134c9
Jack Seigerman <133878087+jackSeigerman@users.noreply.github.com>
2024-02-27 21:04:03 -0600

    fixed twine upload

--- graphicsLib/setup.py
+++ graphicsLib/setup.py
@@ -1,14 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='cs1-graphics',
-    version='0.1',
-    packages=['my_graphics'],
-    install_requires=['pygame'],
+    version='1.2',
+    packages=find_packages(),
+    install_requires=['turtle'],
     author='Jack Seigerman',
     author_email='jacks3ds@gmail.com',
-    description='A simple Python graphics library',
+    description='python 3 drawing library that takes calls similar to the cs1-graphics library and outputs with the '
+                'turtle library',
     long_description_content_type='text/markdown',
+    long_description='Please keep in mind that this is still a work in progress. This project was made because the '
+                     'pre existing cs1 graphics library does not work with python3. This library takes calls that '
+                     'would work with cs1.graphics and renders the image in turtle instead.',
     url='https://github.com/jackSeigerman/cs1-graphics',
     classifiers=[
         'Programming Language :: Python :: 3',
